b0e3e4a79d56494a35a6023c246bad0f1abd6569
jiro.kirameki <taro.kirameki@techacademy.jp>
2018-08-03 02:18:43 +0000

    Bootstrap second commit

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -13,23 +13,23 @@
         </ul>
     @endif
 
-<div class="row">
+<div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
-<div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+<div class="form-group">
         {!! Form::label('status', 'ステータス:') !!}
         {!! Form::text('status', null, ['class' => 'form-control']) !!}
 </div>
 
-<div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
+<div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content', null, ['class' => 'form-control']) !!}
 </div>
 
 {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
+{!! Form::close() !!}
 
-    {!! Form::close() !!}
-</div>
 </div>
 
 @endsection
