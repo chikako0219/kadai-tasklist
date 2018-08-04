4943952df1078bdbf34b41d87e985a9e7d81da39
jiro.kirameki <taro.kirameki@techacademy.jp>
2018-08-04 01:08:04 +0000

    Bootstrap commit 4

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -12,6 +12,8 @@
         </ul>
     @endif
 
+<div class="container">
+<div class="row">
 
 <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 
@@ -31,5 +33,8 @@
 	{!! Form::close() !!}
 
 </div>
+</div>
+</div>
+
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -13,6 +13,9 @@
         </ul>
     @endif
 
+<div class="container">
+<div class="row">
+
 <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
 
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
@@ -30,6 +33,9 @@
 {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
 {!! Form::close() !!}
 
+</div>
+
+</div>
 </div>
 
 @endsection
